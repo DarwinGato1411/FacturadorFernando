0b8ad71119e97c09892ac6fa60a207e00fff31d7
Breiner <jbpaig@gmail.com>
2022-08-18 16:27:17 -0500

    asdfa

--- FacturadorFernando/web/js/inicio.js
+++ FacturadorFernando/web/js/inicio.js
@@ -10,6 +10,47 @@ const deleteTags = (node) => {
   list.forEach((e) => e.classList.remove("z-label"));
 };
 
+const openLogin = () => {
+  const login = document.querySelector(".btnLogin");
+  const modal = document.querySelector(".login_wrapper");
+  const loginContainer = document.querySelector(".login_container_id");
+  const forgot = document.querySelector(".forgot");
+
+  const openModal = () => {
+    loginContainer.style.display = "flex";
+  };
+
+  const closeModal = (e) => {
+    if (!modal.contains(e.target) && e.target !== login) {
+      loginContainer.style.display = "none";
+    }
+  };
+  login.addEventListener("click", openModal);
+  document.addEventListener("click", (e) => closeModal(e));
+  forgot.addEventListener("click", () => {
+    loginContainer.style.display = "none";
+  });
+};
+
+const showPassword = () => {
+  const password = document.querySelector(".input_password");
+  const icon = document.querySelector(".icon_eye");
+
+  const toggleShowPass = () => {
+    if (password.type === "password") {
+      password.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      password.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    }
+  };
+
+  icon.addEventListener("click", toggleShowPass);
+};
+
 const stopLoading = () => {
   const loading = document.querySelector(".loading_inicio");
   loading.style.display = "none";
@@ -19,7 +60,9 @@ const stopLoading = () => {
   document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       deleteTags(".wrapper_inicio"); //inicio.zul
+      openLogin();
+      showPassword();
       stopLoading();
-    }, 2000);
+    }, 100);
   });
 })();
