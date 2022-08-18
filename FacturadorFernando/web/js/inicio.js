d8ce9238f1656d14b7d68e70c83e981f0dd121fe
Breiner <jbpaig@gmail.com>
2022-08-18 13:47:06 -0500

    v1

---
+++ FacturadorFernando/web/js/inicio.js
@@ -0,0 +1,25 @@
+const deleteTags = (node) => {
+  /*
+   *  Se eliminan todas las etiquetas que contengan z-label
+   *    en el nodo padre pasado por argumento
+   */
+
+  const container = document.querySelector(node);
+
+  const list = Array.from(container.querySelectorAll(".z-label"));
+  list.forEach((e) => e.classList.remove("z-label"));
+};
+
+const stopLoading = () => {
+  const loading = document.querySelector(".loading_inicio");
+  loading.style.display = "none";
+};
+
+(() => {
+  document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+      deleteTags(".wrapper_inicio"); //inicio.zul
+      stopLoading();
+    }, 2000);
+  });
+})();
