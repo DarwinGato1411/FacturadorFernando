500556dcfb354244c0699edb7d6e6b204c2f2016
Breiner <jbpaig@gmail.com>
2022-08-20 19:00:27 -0500

    se termino de configurar el proyecto de pichincha

--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/vistas/RotacionProducto_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/vistas/RotacionProducto_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-12T13:10:37")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-08-20T18:48:08")
 @StaticMetamodel(RotacionProducto.class)
 public class RotacionProducto_ { 
 
