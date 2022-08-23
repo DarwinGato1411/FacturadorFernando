737c22ec828aa8f6edd35f29b2e88bed58644907
Breiner <jbpaig@gmail.com>
2022-08-23 10:33:03 -0500

    se hace responsive el sistema y se cambio el jdbc

--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/vistas/Acumuladopordia_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/vistas/Acumuladopordia_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-08-20T18:48:08")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-08-23T10:27:30")
 @StaticMetamodel(Acumuladopordia.class)
 public class Acumuladopordia_ { 
 
