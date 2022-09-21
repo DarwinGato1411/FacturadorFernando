ce9cbbab3afa31c860f6d7115d2b82c76fb4acb2
Breiner <jbpaig@gmail.com>
2022-09-20 19:45:28 -0500

    arreglos

--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Referencia_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Referencia_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-12T13:10:37")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-09-20T19:18:28")
 @StaticMetamodel(Referencia.class)
 public class Referencia_ { 
 
--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Retencionporcasillero_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Retencionporcasillero_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-12T13:10:37")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-09-20T19:18:28")
 @StaticMetamodel(Retencionporcasillero.class)
 public class Retencionporcasillero_ { 
 
