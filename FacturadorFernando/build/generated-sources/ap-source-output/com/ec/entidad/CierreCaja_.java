51dc14fa4a169c9ebba285d73be6c05d68edf503
NanoDan29 <jowell29jm@gmail.com>
2022-09-29 14:27:33 -0500

    agregados estilos

--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/CierreCaja_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/CierreCaja_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-09-20T20:19:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-09-29T12:52:37")
 @StaticMetamodel(CierreCaja.class)
 public class CierreCaja_ { 
 
--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Usuario_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Usuario_.java
@@ -13,7 +13,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-09-20T20:19:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-09-29T12:52:37")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
