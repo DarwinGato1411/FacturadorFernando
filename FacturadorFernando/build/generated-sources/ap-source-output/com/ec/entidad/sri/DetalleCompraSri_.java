a5e4716af00b8e3442a972e853e01c59a845f963
NanoDan29 <jowell29jm@gmail.com>
2022-10-01 17:01:49 -0500

    arreglado favicon y reporter

--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/sri/DetalleCompraSri_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/sri/DetalleCompraSri_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-10-01T02:00:46")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-10-01T16:59:40")
 @StaticMetamodel(DetalleCompraSri.class)
 public class DetalleCompraSri_ { 
 
