68f9ce84b2a6753eaf287ed6b5c040ca8fbc307a
NanoDan29 <jowell29jm@gmail.com>
2022-10-18 10:29:37 -0500

    corros empresa

--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Subcategoria_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Subcategoria_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-10-14T11:13:17")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-10-18T01:16:03")
 @StaticMetamodel(Subcategoria.class)
 public class Subcategoria_ { 
 
