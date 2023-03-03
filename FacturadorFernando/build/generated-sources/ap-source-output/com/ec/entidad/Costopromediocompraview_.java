bad1a105666c742a8ff853b7696e394d26c93705
NanoDan29 <jowell29jm@gmail.com>
2023-03-03 00:56:32 -0500

    cambios en archivo utils y en metodso de clientes y proveedores

--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Costopromediocompraview_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Costopromediocompraview_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-02-01T11:18:09")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-03T00:50:09")
 @StaticMetamodel(Costopromediocompraview.class)
 public class Costopromediocompraview_ { 
 
--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Producto_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Producto_.java
@@ -13,7 +13,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-02-01T11:18:09")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-03T00:50:09")
 @StaticMetamodel(Producto.class)
 public class Producto_ { 
 
