50c3a07a9017221e46d9a404f63308b281b40474
NanoDan29 <jowell29jm@gmail.com>
2023-03-19 23:56:38 -0500

    corrección super admin, agregada busqueda en vista principal, correcion envios de correos, correccion agregar usuario desde admin

--- FacturadorFernando/src/java/com/ec/controlador/GestionUsuarios.java
+++ FacturadorFernando/src/java/com/ec/controlador/GestionUsuarios.java
@@ -10,15 +10,21 @@ import com.ec.seguridad.EnumSesion;
 import com.ec.seguridad.UserCredential;
 import com.ec.servicio.ServicioTipoAmbiente;
 import com.ec.servicio.ServicioUsuario;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import javax.activation.MimetypesFileTypeMap;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Session;
 import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Filedownload;
 
 /**
  *
@@ -28,9 +34,11 @@ public class GestionUsuarios {
 
     ServicioUsuario servicioUsuario = new ServicioUsuario();
     private List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+    private List<Tipoambiente> listaTipoambientes = new ArrayList<Tipoambiente>();
 
-    
-   
+    private String amCodigo = "2";
+    private String nombreUsuario = "";
+
     UserCredential credential = new UserCredential();
     private Tipoambiente amb = new Tipoambiente();
     private String amRuc = "";
@@ -43,14 +51,21 @@ public class GestionUsuarios {
         Session sess = Sessions.getCurrent();
         credential = (UserCredential) sess.getAttribute(EnumSesion.userCredential.getNombre());
 //        amRuc = credential.getUsuarioSistema().getUsuRuc();
-        amb = servicioTipoAmbiente.findALlTipoambientePorUsuario(credential.getUsuarioSistema());
-      
-        cosultarUsuarios("");
+
+        consultarUsuarios();
+//        cosultarUsuarios("");
     }
 
-  
-  
-   
+    private void consultarUsuarios() {
+        listaTipoambientes = servicioTipoAmbiente.findALlTipoambientePorUsuarioAdm(nombreUsuario, amCodigo);
+    }
+
+    @Command
+    @NotifyChange("listaTipoambientes")
+    public void consultarUsuariosPorCodigo() {
+        consultarUsuarios();
+    }
+
 
     /*ADMINISTRAR USUARIO*/
     private void cosultarUsuarios(String buscar) {
@@ -86,6 +101,25 @@ public class GestionUsuarios {
         cosultarUsuarios("");
     }
 
+    
+    @Command
+    public void descargarFirma(@BindingParam("valor") Tipoambiente amb) {
+
+        try {
+            String filePath = amb.getAmDirBaseArchivos() + File.separator + amb.getAmFolderFirma() + File.separator + amb.getAmDirFirma();
+            File dosfile = new File(filePath);
+            if (dosfile.exists()) {
+                FileInputStream inputStream = new FileInputStream(dosfile);
+                Filedownload.save(inputStream, new MimetypesFileTypeMap().getContentType(dosfile), dosfile.getName());
+            } else {
+                Clients.showNotification("La firma no fue cargada", Clients.NOTIFICATION_TYPE_INFO, null, "middle_center", 3000, true);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR AL DESCARGAR EL ARCHIVO" + e.getMessage());
+        }
+
+    }
+    
     public UserCredential getCredential() {
         return credential;
     }
@@ -100,6 +134,30 @@ public class GestionUsuarios {
 
     public void setEsVisisible(Boolean esVisisible) {
         this.esVisisible = esVisisible;
+    }
+
+    public String getAmCodigo() {
+        return amCodigo;
+    }
+
+    public void setAmCodigo(String amCodigo) {
+        this.amCodigo = amCodigo;
+    }
+
+    public List<Tipoambiente> getListaTipoambientes() {
+        return listaTipoambientes;
+    }
+
+    public void setListaTipoambientes(List<Tipoambiente> listaTipoambientes) {
+        this.listaTipoambientes = listaTipoambientes;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
     }
 
 }
