dfd2b107377dea1a92d337847b657fe8350a9d64
Darwin <darwinvinicio14_11@hotmail.com>
2023-05-22 23:36:48 -0500

    Darwin

--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/DetalleKardex_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/DetalleKardex_.java
@@ -10,7 +10,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-02-01T11:18:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-22T23:31:50")
 @StaticMetamodel(DetalleKardex.class)
 public class DetalleKardex_ { 
 
--- FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Gatos_.java
+++ FacturadorFernando/build/generated-sources/ap-source-output/com/ec/entidad/Gatos_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-02-01T11:18:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-22T23:31:50")
 @StaticMetamodel(Gatos.class)
 public class Gatos_ { 
 
