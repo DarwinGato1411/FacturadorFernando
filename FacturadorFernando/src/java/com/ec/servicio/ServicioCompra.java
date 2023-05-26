aeaba9f60217e31ba1ad92749519c25972d79cad
NanoDan29 <jowell29jm@gmail.com>
2023-05-26 12:25:18 -0500

    correccion busqueda factaras de compra por nFactura y nombre proveedor

--- FacturadorFernando/src/java/com/ec/servicio/ServicioCompra.java
+++ FacturadorFernando/src/java/com/ec/servicio/ServicioCompra.java
@@ -110,11 +110,11 @@ public class ServicioCompra {
             DetalleCompra ingreso = null;
             for (DetalleCompraUtil item : detalleCompra) {
                 ingreso = new DetalleCompra(item.getCantidad(),
-                            item.getDescripcion(),
-                            item.getSubtotal(),
-                            item.getTotal(),
-                            compra,
-                            item.getProducto());
+                        item.getDescripcion(),
+                        item.getSubtotal(),
+                        item.getTotal(),
+                        compra,
+                        item.getProducto());
                 ingreso.setDetValorInicial(item.getCantidad());
                 ingreso.setDetFactor(item.getFactor());
                 ingreso.setIprodCantidad(item.getTotalTRanformado());
@@ -129,15 +129,19 @@ public class ServicioCompra {
 
     }
 
-    public List<CabeceraCompra> findCabProveedor(String valor) {
+    public List<CabeceraCompra> findCabProveedor(String proveedor, Tipoambiente codTipoambiente) {
 
         List<CabeceraCompra> listaCabeceraCompras = new ArrayList<CabeceraCompra>();
         try {
             //Connection connection = em.unwrap(Connection.class);
             em = HelperPersistencia.getEMF();
             em.getTransaction().begin();
-            Query query = em.createNamedQuery("CabeceraCompra.findCabProveedor", CabeceraCompra.class);
-            query.setParameter("cabProveedor", "%" + valor + "%");
+
+            //Query query = em.createQuery("SELECT c FROM CabeceraCompra c WHERE c.cabProveedor LIKE :cabProveedor ORDER BY c.cabFechaEmision DESC");
+            Query query = em.createQuery("SELECT c FROM CabeceraCompra c WHERE c.cabProveedor LIKE :cabProveedor AND c.codTipoambiente=:codTipoambiente ORDER BY c.cabFechaEmision DESC");
+            
+            query.setParameter("cabProveedor", "%"+proveedor+"%");
+            query.setParameter("codTipoambiente", codTipoambiente);
             listaCabeceraCompras = (List<CabeceraCompra>) query.getResultList();
             em.getTransaction().commit();
         } catch (Exception e) {
@@ -149,7 +153,7 @@ public class ServicioCompra {
         return listaCabeceraCompras;
     }
 
-    public List<CabeceraCompra> findByBetweenFecha(Date incio, Date fin,Tipoambiente codTipoambiente) {
+    public List<CabeceraCompra> findByBetweenFecha(Date incio, Date fin, Tipoambiente codTipoambiente) {
 
         List<CabeceraCompra> listaCabeceraCompras = new ArrayList<CabeceraCompra>();
         try {
@@ -171,15 +175,17 @@ public class ServicioCompra {
         return listaCabeceraCompras;
     }
 
-    public List<CabeceraCompra> findByNumeroFactura(String cabNumFactura) {
+    public List<CabeceraCompra> findByNumeroFactura(String cabNumFactura, Tipoambiente codTipoambiente) {
 
         List<CabeceraCompra> listaCabeceraCompras = new ArrayList<CabeceraCompra>();
         try {
             //Connection connection = em.unwrap(Connection.class);
             em = HelperPersistencia.getEMF();
             em.getTransaction().begin();
-            Query query = em.createQuery("SELECT c FROM CabeceraCompra c WHERE c.cabNumFactura LIKE :cabNumFactura ORDER BY c.cabNumFactura DESC");
+            Query query = em.createQuery("SELECT c FROM CabeceraCompra c WHERE c.cabNumFactura LIKE :cabNumFactura AND c.codTipoambiente=:codTipoambiente ORDER BY c.cabNumFactura DESC");
+            
             query.setParameter("cabNumFactura", "%" + cabNumFactura + "%");
+            query.setParameter("codTipoambiente", codTipoambiente);
             listaCabeceraCompras = (List<CabeceraCompra>) query.getResultList();
             em.getTransaction().commit();
         } catch (Exception e) {
